c7352664b02869b8c01ed3609d1493c04d5f2e0e
asmaamoali <Asmaamoali>
2023-09-07 16:26:27 +0200

    up

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" add two integers method"""
+
+
+def add_integer(a, b=98):
+    """  add two integres 
+    
+    a: the first number 
+    b: the second number 
+
+    raises 
+    typeError : if a,b is not int or float 
+
+    return 
+    sum of two number 
+    """
+
+    if type(a) not in (int , float):
+        raise TypeError('a must be an integer')
+    if type(b) not in (int,float):
+        raise TypeError('b must be an integer')
+    return int(a) + int(b)
