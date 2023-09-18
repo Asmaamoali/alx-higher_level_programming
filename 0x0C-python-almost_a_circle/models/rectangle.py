2bcf83749c0029167dfbe42a2df4d89cdd137516
asmaamoali <Asmaamoali>
2023-09-18 17:52:31 +0200

    up

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Defines a rectangle class."""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Represent a rectangle."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
+        """
+    
+        self.width = width
+        self.height = height
+        self.x = x 
+        self.y = y 
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """Set/get the width of the Rectangle."""
+        return self.__width
+    
+    @property
+    def height(self):
+        """Set/get the height of the Rectangle."""
+        return self.__height
+    
+    @property
+    def x (self):
+        """Set/get the x of the Rectangle."""
+        return self.__x 
+       
+    @property
+    def y(self):
+        """Set/get the y of the Rectangle."""
+        return self.__y
